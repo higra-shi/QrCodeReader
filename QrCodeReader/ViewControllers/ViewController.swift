e9c513b2d76f879e801e46abb0f429a5d2b817bb
Shusaku HARADA <cawasemi@gmail.com>
2015-03-01 10:56:59 +0900

    create project

---
+++ QrCodeReader/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QrCodeReader
+//
+//  Created by 原田 周作 on 2015/03/01.
+//  Copyright (c) 2015年 Shusaku HARADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
